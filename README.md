bd6dd8cc96fd3b34ed86131283367f8fd6874316
Vladislav Buldakov <vladislav.buldakov@openmedia.co>
2021-03-03 16:25:32 +0300

    Init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # emojifyer
+
+### Instaling
+
+```bash
+python3 -m venv env
+/env/bin/activate
+pip install -r reqirements.pip
+```
+
+# Using
+
+```bash
+export FLASK_APP=app.py
+flask run
+```
+/emojify?url=https://lifehacker.ru/uborka-kvartiry/
---
+++ app.py
@@ -0,0 +1,71 @@
+import itertools
+import re
+from typing import Iterator
+
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Comment, PageElement
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+def is_elem_visible(page_element: PageElement) -> bool:
+    """Checks if bs4 element is visible text
+    """
+    visible_elems = ['style', 'script', 'head', 'title', 'meta', '[document]']
+    if page_element.parent.name in visible_elems:
+        return False
+    if isinstance(page_element, Comment):
+        return False
+    return True
+
+
+def emoji_generator() -> Iterator[str]:
+    """Infinitely returns next emoji in emoji_list
+    """
+    emoji_list = ['\U0001F600', '\U0001F642', '\U0001F61D', '\U0001F92B']
+    for emoji in itertools.cycle(emoji_list):
+        yield emoji
+
+
+@app.route('/emojify/')
+def emojify():
+    url = request.args.get('url', None)
+    if not url:
+        return ('Provide <url> param as request arg, '
+                'like /?url=https://example.com'), 400
+
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+    except Exception as err:
+        print('Url exception', repr(err))
+        return 'Url exception', 400
+
+    emoji_iter = emoji_generator()
+    try:
+        visible_elems = filter(
+            lambda elem: len(elem) > 5 and is_elem_visible(elem),
+            soup.findAll(text=True))
+
+        for elem in visible_elems:
+            if not elem.string:
+                continue
+
+            replace_to = '___emoji___'
+            emoji_str = re.sub(r'(\b\w{6}\b)', f'\\1{replace_to}', elem.string)
+            while replace_to in emoji_str:
+                emoji_str = emoji_str.replace(replace_to, next(emoji_iter), 1)
+
+            elem.string.replace_with(emoji_str)
+        return str(soup)
+    except Exception as err:
+        print('Replacement error', repr(err))
+        return 'Error while replacing', 400
+
+
+@app.route('/')
+def home():
+    return 'Using: /emojify/?url=<url>', 200
